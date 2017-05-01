d6a3567cec3ecc5939f6bc0b6361051e23fc5a92
Vipul999ujawane <vipul999ujawane@gmail.com>
2017-05-01 20:18:29 +0530

    Home page done with all animations

--- anim.js
+++ anim.js
@@ -1,3 +1,8 @@
 $( "#title" ).delay(1000).slideUp( "slow",function()
 {$("#dept").slideDown("slow");
 });
+$(".click").click(function(){
+  $("#dept").slideUp("slow",function(){
+    $("#year").slideDown("slow");
+  });
+});
